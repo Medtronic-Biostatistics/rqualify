c6ce40997896637981f8e52111ac6f1aea56b975
donaldmusgrove <donniemusgrove@gmail.com>
2026-03-26 11:58:40 -0500

    Example condition fix

--- R/rqualify_setup.R
+++ R/rqualify_setup.R
@@ -22,7 +22,7 @@
 #' @return A list with the installed Pandoc and TinyTeX versions, though the primary
 #'  purpose of this function is its side effects, setting up TinyTeX and Pandoc as specified.
 #'   
-#' @examples
+#' @examplesIf tinytex::is_tinytex() && pandoc::pandoc_available() && "grfext" %in% tinytex::tl_pkgs(only_installed = TRUE)
 #' rqualify_setup(setup_tinytex = FALSE,
 #'                setup_pandoc  = FALSE,
 #'                verbose       = FALSE)
